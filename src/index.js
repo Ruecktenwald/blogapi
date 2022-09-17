6735300fadf851660b136a2ccda0929a12ab1d34
Peter Ruecktenwald <ruecktenwald@gmail.com>
2022-09-17 10:58:05 -0400

    add scaffold with added components and updated index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import App from './App';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
